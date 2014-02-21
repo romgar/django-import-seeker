ea33c59d1d6e0a0291ac89d3831f4b5b8af0dd76
Natsu- <romain.garrigues.cs@gmail.com>
2014-02-21 18:16:08 +0100

    Adding post for dynamic string search

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import os
 
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 from introspection import DjangoImportSeeker
@@ -11,14 +13,23 @@ from introspection import DjangoImportSeeker
 
 @app.route('/')
 def server():
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'dummy_settings'
+    return render_template('home.html')
 
-    djangoIS = DjangoImportSeeker('render', 'django')
 
-    result = djangoIS.process_seek()
-    return render_template('home.html', result=result)
+@app.route('/search', methods=['POST'])
+def search():
+
+    result = []
+    if request.method == 'POST':
+        string_to_find = request.form['string_to_find']
+
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'dummy_settings'
+
+        djangoIS = DjangoImportSeeker(string_to_find, 'django')
+
+        result = djangoIS.process_seek()
+
+    return render_template('home.html', result=" ".join(result))
 
 
 if __name__ == '__main__':
